c270c9592931821bd7b72ab8691dba0dda1e2e3f
BhuwanChhimal <bhuwanchhimal@gmail.com>
2025-02-01 19:49:26 +0545

    formal

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const db = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(db, { //connecting to mongodb database
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
